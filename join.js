0fea2f6fbe3ff09eaf97d6a5bfefb9a48c224d97
denitsastancheva <denitsastancheva64@gmail.com>
2026-01-19 23:36:24 +0200

    Един от нас

--- join.js
+++ join.js
@@ -2,6 +2,7 @@ function validateForm() {
     let name = document.getElementById("fname").value;
     let lastname = document.getElementById("lname").value;
     let email = document.getElementById("email").value;
+    let reason = document.getElementById("reason").value;
     let password = document.getElementById("password").value;
 
 
@@ -20,12 +21,15 @@ function validateForm() {
         return false;
     }
 
-    if (!password || password.length < 7 || !/\d/.test(password) || !/[A-Z]/.test(password) || !!/[a-z]/.test(password) || !/[!@#$%^&*(),.?":{}|<>]/.test(password)){
-        alert("")
+    if (reason.length < 15) {
+        alert("Трябва да е поне 15 символа");
         return false;
     }
 
-    //reason
+    if (!password || password.length < 7 || !/\d/.test(password) || !/[A-Z]/.test(password) || !/[a-z]/.test(password) || !/[!@#$%^&*(),.?":{}|<>]/.test(password)){
+        alert("Паролата трябва да съдържа:\n - поне 7 знака;\n - големи/малки букви;\n - числа;\n - специални символи;")
+        return false;
+    }
 
     else return alert("Успешно се регистрирахте!");
 }
